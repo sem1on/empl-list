f8e03b08d59723dded1b2bc7c3aacc719eae7125
sem1on <sgorlov21@gmail.com>
2022-02-10 02:32:00 +0300

    Добавлен функционал по удалению и добавлению новых сотрудников в БД

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,63 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name:'Jon S.', salary: 800, increase: false, id: 1},
+                {name:'Semion G.', salary: 4600, increase: true, id: 2},
+                {name:'Mick D.', salary: 2100, increase: false, id: 3},
+                {name:'Garry N.', salary: 1700, increase: false, id: 4},
+            ]
+        }
+        this.maxId = 5;
+    }
 
-    const data = [
-        {name:'Jon S.', salary: 800, increase: false, id: 1},
-        {name:'Semion G.', salary: 4600, increase: true, id: 2},
-        {name:'Mick D.', salary: 2100, increase: false, id: 3},
-        {name:'Garry N.', salary: 1700, increase: false, id: 4},
-    ]
+    deletItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex((elem) => elem.id === id);
+            // const before = data.slice(0, index);
+            // const after = data.slice(index +1)
+            // const newData = [...before, ...after]
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    return (
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) =>{
+            const newArray = [...data, newItem]
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    render() {
+        return (
         <div className='app'>
             <AppInfo/>
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>   
             </div>
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
+            <EmployeesList 
+                data={this.state.data}
+                onDelete={this.deletItem}/>
+            <EmployeesAddForm
+                onAdd={this.addItem}/>
         </div>
-    )
+        )
+    }
 }
 
 export default App;
